e34e997d5ce540cde540a5d784e07c1e2fddf45c
joriszwart.nl <git@joriszwart.nl>
2015-03-19 21:45:48 +0100

    print a list of id strings from the generated documentation xml

--- Program.cs
+++ Program.cs
@@ -3,13 +3,67 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace dokumentasi
 {
     class Program
     {
+        /// <summary>
+        /// The entry point of the application.
+        /// </summary>
+        /// <param name="args">command line parameters</param>
         static void Main(string[] args)
         {
+            var doc = XDocument.Load("dokumentasi.xml");
+            var members = doc.Descendants("member");
+            foreach(var member in members)
+            {
+                string name = member.Attribute("name").Value;
+                switch(name[0])
+                {
+                    case 'E':
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.Write("event: ");
+                        break;
+
+                    case 'F':
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.Write("field: ");
+                        break;
+
+                    case 'M':
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.Write("member: ");
+                        break;
+
+                    case 'N':
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.Write("namespace: ");
+                        break;
+
+                    case 'P':
+                        Console.ForegroundColor = ConsoleColor.DarkGray;
+                        Console.Write("property: ");
+                        break;
+
+                    case 'T':
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                        Console.Write("class/interface/struct/enum/delegate: ");
+                        break;
+
+                    case '!':
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                        Console.Write("resolve error: ");
+                        break;
+
+                    default:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write("unknown: ");
+                        break;
+                }
+                Console.WriteLine(name);
+            }
         }
     }
 }
