e54685b765f9d343afb84c6aa14882438f3823b0
joriszwart.nl <git@joriszwart.nl>
2015-03-20 21:59:37 +0100

    Descriptive namespace and class

--- test.cs
+++ test.cs
@@ -1,37 +1,37 @@
 ///
 ///
 using System;
-namespace N  // "N:N"
+namespace SomeNamespace  // "N:SomeNamespace"
 {
    public class E : Exception {};
    ///
    ///
-   public /* unsafe */ class X    // "T:N.X"
+   public /* unsafe */ class SomeClass    // "T:SomeNamespaceX"
    {
       /// <summary>
       /// 
       /// </summary>
-      public X(){}   // "M:N.X.#ctor"
-      // public X(){} // "M:N.X.#cctor", a class constructor
+      public SomeClass(){}   // "M:SomeNamespace.SomeClass.#ctor"
+      // public SomeClass(){} // "M:SomeNamespace.SomeClass.#cctor", a class constructor
       /// <summary>
       /// 
       /// </summary>
       /// <param name="i"></param>
-      public X(int i){}  // "M:N.X.#ctor(System.Int32)"
+      public SomeClass(int i){}  // "M:SomeNamespaceX.#ctor(System.Int32)"
       /// <summary>
       /// 
       /// </summary>
-      ~X(){}  // "M:N.X.Finalize", destructor's representation in metadata
+      ~SomeClass(){}  // "M:SomeNamespace.SomeClass.Finalize", destructor's representation in metadata
       
       /// <summary>
       /// 
       /// </summary>
-      public string q;  // "F:N.X.q"
+      public string q;  // "F:SomeNamespace.SomeClass.q"
       /// <summary>
       /// 
       /// </summary>
       /// <returns></returns>
-      public const double PI = 3.14;  // "F:N.X.PI"
+      public const double PI = 3.14;  // "F:SomeNamespace.SomeClass.PI"
       /// <summary>
       /// 
       /// </summary>
@@ -39,7 +39,7 @@ namespace N  // "N:N"
       /// <param name="y"></param>
       /// <param name="z"></param>
       /// <returns></returns>
-      public int f(){return 1;}  // "M:N.X.f"
+      public int f(){return 1;}  // "M:SomeNamespace.SomeClass.f"
       /// <summary>
       /// 
       /// </summary>
@@ -47,46 +47,46 @@ namespace N  // "N:N"
       /// <param name="array"></param>
       /// <returns></returns>
 //      public int bb(string s, ref int y, void * z){return 1;}
-      // "M:N.X.bb(System.String,System.Int32@,=System.Void*)"
+      // "M:SomeNamespace.SomeClass.bb(System.String,System.Int32@,=System.Void*)"
       /// <summary>
       /// 
       /// </summary>
       /// <param name="x"></param>
       /// <param name="xx"></param>
       /// <returns></returns>
-      public int gg(short[] array1, int[,] array){return 0;} 
-      // "M:N.X.gg(System.Int16[], System.Int32[0:,0:])"
+      public int gg(short[] array1, int[,] array){return 0;}
+      // "M:SomeNamespace.SomeClass.gg(System.Int16[], System.Int32[0:,0:])"
       /// <summary>
       /// 
       /// </summary>
-      public static X operator+(X x, X xx){return x;} // "M:N.X.op_Addition(N.X,N.X)"
+      public static SomeClass operator+(SomeClass x, SomeClass xx){return x;} // "M:SomeNamespace.SomeClass.op_Addition(SomeNamespace.SomeClass,SomeNamespace.SomeClass)"
       /// <summary>
       /// 
       /// </summary>
-      public int prop {get{return 1;} set{}} // "P:N.X.prop"
+      public int prop {get{return 1;} set{}} // "P:SomeNamespace.SomeClass.prop"
       /// <summary>
       /// 
       /// </summary>
       ///
-      public event D d; // "E:N.X.d"
+      public event D d; // "E:SomeNamespace.SomeClass.d"
       /// <summary>
       /// 
       /// </summary>
-      public int this[string s]{get{return 1;}} // "P:N.X.Item(System.String)"
+      public int this[string s]{get{return 1;}} // "P:SomeNamespace.SomeClass.Item(System.String)"
       /// <summary>
       /// 
       /// </summary>
-      public class Nested{} // "T:N.X.Nested"
+      public class Nested{} // "T:SomeNamespace.SomeClass.Nested"
       /// <summary>
       /// 
       /// </summary>
-      public delegate void D(int i); // "T:N.X.D"
+      public delegate void D(int i); // "T:SomeNamespaceX.D"
       /// <summary>
       /// 
       /// </summary>
       /// <param name="x"></param>
       /// <returns></returns>
-      public static explicit operator int(X x){return 1;} 
-      // "M:N.X.op_Explicit(N.X)~System.Int32"
+      public static explicit operator int(SomeClass x){return 1;} 
+      // "M:SomeNamespace.SomeClass.op_Explicit(SomeNamespace.SomeClass)~System.Int32"
    }
 }
